ddcfe36d8d10cbcf714da325c1f8f69b2a8b5a7e
idealik <shavl.mark@yandex.ru>
2019-05-12 16:38:13 +0300

    first index

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
-var http = require('http');
+const http = require('http');
 
-console.log('Server started');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+var stdin = process.openStdin();
+
+rl.question('Who are u? admin/user ', (data) => {
+    // TODO: Log the answer in a database
+    switch (data) {
+        case "admin": {
+            console.log("Enter password");
+            credentials = data;
+            rl.question('Password: ', (password) => {
+                if (password == "123") {
+                    console.log("Authorized " + password.toString());
+                    rl.setPrompt('Your request> ');
+                    rl.prompt();
+                    rl.on('line', function (request) {
+                        if (request === "close") rl.close();
+                        createRequest(request);
+                    }).on('close', function () {
+                        console.log("CLOSE");
+                        process.exit(0);
+                    });
+
+                } else {
+                    console.log("NAH");
+                    process.exit(0);
+                }
+            });
+            break;
+        }
+        case "user": {
+            console.log("Hi user!")
+            credentials = data;
+            break;
+        }
+        default:
+            break;
+    }
+});
+
+function createRequest() {
+    console.log("Nice!");
+    rl.prompt();
+}
+
+
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('My first server!');
 }).listen(8080);
