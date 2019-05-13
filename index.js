807515b6af4cafa57a42a07024a662b0cdb24ff7
idealik <shavl.mark@yandex.ru>
2019-05-13 12:20:51 +0300

    The architecture has been changed

--- index.js
+++ index.js
@@ -11,18 +11,34 @@ const rl = readline.createInterface({
 const ADMIN = "admin";
 const USER = "user";
 const CREATE = "create";
+const DELETE = "delete";
 const USE = "use";
 
-rl.question('create/use role? ', (data) => {
+// роли будут подгружаться из базы данных
+rl.question('What is your role admin/user ?', (data) => {
+    //set role
     data = data.toLowerCase();
-
     switch (data) {
-        case USE: {
-            getRole();
+        case ADMIN: {
+            console.log("Enter password");
+            rl.question('Password: ', (password) => {
+                if (password == "123") {
+                    credentials = data;
+                    console.log("Authorized " + password.toString());
+                    startReadRequest();
+
+                } else {
+                    console.log("Password is wrong");
+                    process.exit(0);
+                }
+            });
             break;
         }
-        case CREATE: {
-            createRole();
+        case USER: {
+            //можно загружать права, которыми он обладает
+            console.log("Hi user! You have this rules: ...");
+            credentials = data;
+            startReadRequest();
             break;
         }
         default:
@@ -30,50 +46,25 @@ rl.question('create/use role? ', (data) => {
     }
 });
 
-
-function getRole() {
-    // роли будут подгружаться из базы данных
-    rl.question('What is your role admin/user ?', (data) => {
-        //set role
-        data = data.toLowerCase();
-        switch (data) {
-            case ADMIN: {
-                console.log("Enter password");
-                rl.question('Password: ', (password) => {
-                    if (password == "123") {
-                        credentials = data;
-                        console.log("Authorized " + password.toString());
-                        startReadRequest();
-                    } else {
-                        console.log("Password is wrong");
-                        process.exit(0);
-                    }
-                });
-                break;
-            }
-            case USER: {
-                //можно загружать права, которыми он обладает
-                console.log("Hi user! You have this rules: ...");
-                credentials = data;
-                startReadRequest();
-                break;
-            }
-            default:
-                break;
-        }
-    });
-
-}
-
-function createRole(){
-    //реализация создания роли
-}
-
 function startReadRequest() {
+
+    let commandToLineRequest = Array()["request", "close", "change role"];
+
+    console.log(); // вывести все команды
     rl.setPrompt('Your request> ');
     rl.prompt();
     rl.on('line', function (request) {
         if (request === "close") rl.close();
+        if (request === "change roles") {
+            if (haveRigthToChangeRole()) {
+                actingWithRole();
+            }
+            else {
+                console.log("Not enough rights");
+            }
+            return;
+        }
+    
         // пока програмно задаем права(фильтры), позже можно будет создавать роль
         // ее права будут заноситься в бд
         // в зависимости от того, за кого мы зашли, будем смотреть права
@@ -87,18 +78,51 @@ function startReadRequest() {
                     createRequest(request);
                 }
                 else {
-                    console.log("u dont have enough rules")
+                    console.log("u dont have enough rights")
                 }
                 break;
             }
             default:
                 break;
         }
+        rl.prompt();
     }).on('close', function () {
         console.log("CLOSE");
         process.exit(0);
     });
 }
+function haveRigthToChangeRole() {
+    return true;
+}
+
+function actingWithRole() {
+    rl.question('create/delete role? ', (data) => {
+        data = data.toLowerCase();
+
+        switch (data) {
+            case CREATE: {
+                createRole();
+                break;
+            }
+            case DELETE: {
+                deleteRole();
+                break;
+            }
+            default:
+                break;
+        }
+    });
+}
+
+function createRole() {
+    console.log("Role created")
+}
+
+function deleteRole() {
+    console.log("Role deleted")
+}
+
+
 //"SELECT * FROM 2BTeam.main"
 
 function createRequest(request) {
@@ -114,7 +138,6 @@ function createRequest(request) {
             });
         con.release();
     });
-    rl.prompt();
 }
 
 function userFilter(request) {
